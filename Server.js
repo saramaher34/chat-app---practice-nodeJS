1b409ea0dedcd60846c5a6ae03ab38cde99e5f3d
saramaher34 <saramaher34@gmail.com>
2021-01-06 17:03:22 +0200

    use socket Io to notify fom server side to client side

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+const { Socket } = require('dgram')
 var app = express()
 //for server to notify client
 var http = require('http').Server(app)
@@ -9,7 +10,7 @@ var io = require('socket.io')(http)
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 // what comes from browser is url encoded
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 var messages = [{ name: 'Aya', message: 'Hey!' },
 { name: 'Joe', message: 'Wlcome!' }]
@@ -18,9 +19,13 @@ app.get('/messages', (req, res) => {
     res.send(messages)
 })
 app.post('/messages', (req, res) => {
-    console.log(req.body)
     messages.push(req.body)
+    io.emit('message', req.body)
     res.sendStatus(200)
+})
+
+io.on("connection", (socket) => {
+    console.log("a user is connected")
 })
 var server = http.listen(4000, () => {
     console.log('server is listening on port', server.address().port
