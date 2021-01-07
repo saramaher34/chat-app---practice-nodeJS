a8fa9adcbb41af30a73c2cb9f3966087b5489349
saramaher34 <saramaher34@gmail.com>
2021-01-07 15:38:29 +0200

    use mongo db to store and get messages

--- Server.js
+++ Server.js
@@ -5,6 +5,7 @@ var app = express()
 //for server to notify client
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 //as middleware
 app.use(express.static(__dirname))
@@ -12,21 +13,39 @@ app.use(bodyParser.json())
 // what comes from browser is url encoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var messages = [{ name: 'Aya', message: 'Hey!' },
-{ name: 'Joe', message: 'Wlcome!' }]
+//if going to production store in a safe configuration file
+var dbURL = 'mongodb+srv://admin:<password>@cluster0.hejeo.mongodb.net/<dbName>?retryWrites=true&w=majority'
+
+var Mesasage = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Mesasage.find({},(err, messages)=>{
+
+        res.send(messages)
+    })
 })
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    var message = new Mesasage(req.body)
+    message.save((err) => {
+        if (err)
+            sendStatus(500)
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
 })
 
 io.on("connection", (socket) => {
     console.log("a user is connected")
 })
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    console.log('mongo connection error:', err)
+})
+
 var server = http.listen(4000, () => {
     console.log('server is listening on port', server.address().port
 
